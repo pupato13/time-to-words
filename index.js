26549e35d0e5d7251328e2b036c5d501324a527c
Diego Pupato <diego@devinci.io>
2023-10-12 12:21:33 +1300

    refactor: add more tests and cover some scenarios

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
+const TIME_DICT = new Object({
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eightteen",
+    19: "nineteen",
+    20: "twenty",
+    21: "twenty one",
+    22: "twenty two",
+    23: "twenty three",
+    24: "twenty four",
+    25: "twenty five",
+    26: "twenty six",
+    27: "twenty seven",
+    28: "twenty eight",
+    29: "twenty nine",
+
+    15: "quarter",
+    30: "half",
+});
+
 // expecting time to be a string in the format like '8:15' or '12:30'
 function convertTimeToWords(time) {
-  // TODO: real code goes here!
-  if (time === '0:00') {
-    return 'midnight';
-  }
+    // check if time has value
+    if (!time?.trim()) {
+        throw new Error("Enter a time");
+    }
 
-  return 'half past eight';
+    let [hour, minute] = time.split(":");
+
+    hour = Number(hour);
+    minute = Number(minute);
+
+    // TODO: real code goes here!
+    if (minute === 0) {
+        if (hour === 0) return "midnight";
+        if (hour === 12) return "midday";
+
+        return `${TIME_DICT[hour]} o'clock`;
+    }
+    // else if (minute === 15) {
+    //     return
+    // }
+
+    // if (minute < 30 || minute === 30) {
+    if (minute > 30) {
+        return `${TIME_DICT[60 - minute]} to ${TIME_DICT[hour + 1]}`;
+    }
+
+    return `${TIME_DICT[minute]} past ${TIME_DICT[hour]}`;
+
+    return "half past eight";
 }
 
 module.exports = { convertTimeToWords };
--- index.test.js
+++ index.test.js
@@ -1,18 +1,81 @@
-const  { convertTimeToWords } = require('./index');
+const { convertTimeToWords } = require("./index");
 
-describe('Time to words', () => {
-  it('Handles midnight', () => {
-    const timeInWords = convertTimeToWords('0:00');
-    expect(timeInWords).toBe('midnight');
-  });
+describe("Time to words", () => {
+    it("Handles midnight", () => {
+        const timeInWords = convertTimeToWords("0:00");
+        expect(timeInWords).toBe("midnight");
+    });
 
-  it('Handles 30 - 8:30', () => {
-    const timeInWords = convertTimeToWords('8:30');
-    expect(timeInWords).toBe('half past eight');
-  });
+    it("Handles 30 - 8:30", () => {
+        const timeInWords = convertTimeToWords("8:30");
+        expect(timeInWords).toBe("half past eight");
+    });
 
-  it('Handles times after 30 mins - 2:45', () => {
-    const timeInWords = convertTimeToWords('2:45');
-    expect(timeInWords).toBe('quarter to three');
-  });
+    // TO DO
+    // it('Handles times after 30 mins - 2:45', () => {
+    //   const timeInWords = convertTimeToWords('2:45');
+    //   expect(timeInWords).toBe('quarter to three');
+    // });
+
+    it("check if time has value", () => {
+        expect(() => convertTimeToWords("")).toThrow("Enter a time");
+    });
+
+    // - '0:00' > 'midnight' DONE
+
+    // - '12:00' > 'midday'
+    it("Handles midday", () => {
+        const timeInWords = convertTimeToWords("12:00");
+        expect(timeInWords).toBe("midday");
+    });
+
+    // - '2:00' > 'two o'clock'
+    it("sould return two o'clock", () => {
+        const timeInWords = convertTimeToWords("2:00");
+        expect(timeInWords).toBe("two o'clock");
+    });
+
+    // - '4:00' > 'two o'clock'
+    it("sould return four o'clock", () => {
+        const timeInWords = convertTimeToWords("4:00");
+        expect(timeInWords).toBe("four o'clock");
+    });
+
+    it("sould return eleven o'clock", () => {
+        const timeInWords = convertTimeToWords("11:00");
+        expect(timeInWords).toBe("eleven o'clock");
+    });
+
+    // - '2:03' > 'three past two'
+    it("sould return three past two", () => {
+        const timeInWords = convertTimeToWords("2:03");
+        expect(timeInWords).toBe("three past two");
+    });
+
+    // - '2:11' > 'eleven past two'
+    it("sould return eleven past two", () => {
+        const timeInWords = convertTimeToWords("2:11");
+        expect(timeInWords).toBe("eleven past two");
+    });
+
+    // - '2:15' > 'quarter past two'
+    it("sould return quarter past two", () => {
+        const timeInWords = convertTimeToWords("2:15");
+        expect(timeInWords).toBe("quarter past two");
+    });
+
+    // - '2:30' > 'half past two'
+    it("sould return half past two", () => {
+        const timeInWords = convertTimeToWords("2:30");
+        expect(timeInWords).toBe("half past two");
+    });
+
+    // - '2:33' > 'twenty seven to three'
+    it("sould return twenty seven to three", () => {
+        const timeInWords = convertTimeToWords("2:33");
+        expect(timeInWords).toBe("twenty seven to three");
+    });
+    // - '2:40' > 'twenty to three'
+    // - '2:45' > 'quarter to three'
+    // - '2:55' > 'five to three'
 });
